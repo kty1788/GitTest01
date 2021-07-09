5f1d92bf74e7d84ad036aa43f281658cfc48ec82
kty1788 <86402835+kty1788@users.noreply.github.com>
2021-07-09 11:19:11 +0900

    Hi 출력

--- GitTest01/src/main/java/com/kh/gittest01/MyController.java
+++ GitTest01/src/main/java/com/kh/gittest01/MyController.java
@@ -9,7 +9,7 @@ public class MyController {
 	
 	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	public String hello() throws Exception {
-		System.out.println("Hello");
+		System.out.println("Hi");
 		return "home";
 	}
 }
